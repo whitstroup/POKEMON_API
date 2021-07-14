5dd0d9fba778694db8825cd5a337b939e715b3dd
Whit Stroup <email@example.com>
2021-07-14 10:51:24 -0500

    FINISHED FOR PUSH

---
+++ PokemonAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+
+namespace PokemonAPI
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+           string pokeURL = "https://pokeapi.co/api/v2/pokemon/";
+
+         
+            //TODO create a new instance of HttpClient called client.
+            //TODO use your client instance to get a response from the poke URL.
+            //TODO go to https://json2csharp.com and convert your json reponse to classes. Create a new class file in visual studio and paste the classes you created on the website.
+            //TODO create a variable that = JsonConvert.DeserializeObject<YourRootClassGoesHere>(yourStringResponseGoesHere);
+            //TODO print the results from your
+
+            //TODO use the pokemon url above and change it to try and call your favorite pokemon.
+            //TODO Use select token to try and grab a couple values from your pokemon and display them.
+        }
+    }
+}
